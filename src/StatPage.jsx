2d872b6f4ba76d4a84f5518b5a0116865c6824bb
Zoltan Sisak <zoltan.sisak@gmail.com>
2022-03-10 16:00:45 +0100

    export

--- src/StatPage.jsx
+++ src/StatPage.jsx
@@ -1,22 +1,45 @@
-import styled from "styled-components"
+/* import styled from "styled-components" */
 import  {useStatQuery, useGetAll} from "./tools/datoCmsTools"
 import {useState} from 'react'
+import { CSVLink as CSVLink_ } from "react-csv"
+import {Button as MuiButton} from "@mui/material"
+import { styled } from '@mui/system'
 
 const StatPage = () => {
 	const [onsite, online, all] = useStatQuery("onsite")
 		
 	const stages = useGetAll("stage")
 	const registrations = useGetAll("registration")
+	console.log("registrations", registrations)
+	
+	const registrationsForExport = registrations?.map(registration => {
+		const {id, name, email, phone, workplace, onsite, stage, vipCode} = registration
+		return {id, name, email, phone, workplace, onsite, stage, vipCode}
+	})
+
+	const headers = [
+		{ label: "id", key: "id" },
+		{ label: "name", key: "name" },
+		{ label: "email", key: "email" },
+		{ label: "phone", key: "phone" },
+		{ label: "workplace", key: "workplace" },
+		{ label: "onsite", key: "onsite" },
+		{ label: "stage", key: "stage" },
+		{ label: "vipCode", key: "vipCode" },
+	  ];
+	console.log("registrationsForExport", registrationsForExport)
 	const breakoutSessions = (stages.map(stage => {
 		const regs = registrations?.filter((reg) => reg.stage===`${stage.id}`)
 		return {name: stage.name, numberOfRegistration: regs.length}
 	}))
-	const onsiteBreakoutSessions = breakoutSessions.filter(bs => bs.numberOfRegistration>0)
 	
-/* 	console.log("data", registrations)
-	const stage1 = registrations?.filter((reg) => reg.stage==="107304832") 
-	console.log("stage1", stage1.length)
- */
+	const onsiteBreakoutSessions = breakoutSessions.filter(bs => bs.numberOfRegistration>0)
+
+	const csvReport = {
+		data: registrationsForExport,
+		headers: headers,
+		filename: 'iok2022_jelentkezesek.csv'
+	  };
 	
 	return (
 		<Container>
@@ -26,11 +49,30 @@ const StatPage = () => {
 			<div>Regisztráció helyszíni részvételre: {onsite?._allRegistrationsMeta.count}</div>
 			<div>&nbsp;</div>
 			{onsiteBreakoutSessions?.map((breakoutSession) => <div>{breakoutSession.name}: {breakoutSession.numberOfRegistration} </div> )}
+			
+			<CSVLink {...csvReport} separator=";" style={{textDecoration:"none"}}><Button variant="contained">Exportálás CSV fájlba</Button></CSVLink>
+			
+
 		</Container>
 	)
 }
 
-const Container = styled.div`
+const Button = styled(MuiButton)(
+	{
+		width: "250px",
+		textDecoration: "none"
+	}
+)
+	
+const CSVLink = styled(CSVLink_)(
+	{
+		marginTop: "10px",
+		width: "250px",
+		textDecoration: "none"
+	}
+)
+
+const Container = styled('div')`
     display: grid;
 	grid-template-columns: auto;
     justify-content:center;
@@ -38,7 +80,8 @@ const Container = styled.div`
     color: white;
     background-color: #47CCD4;
 	padding: 50px;
-    margin-top: -90px
+    margin-top: -90px;
+	a {text-decoration: "none"}	
 `
 
 export default StatPage
